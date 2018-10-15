0fe3ef66c32babbda8368da88f19effab543bb38
459471667 <459471667@qq.com>
2018-10-15 17:28:20 +0800

    1

---
+++ app/components/VideoPlayer.js
@@ -0,0 +1,130 @@
+import React,{ Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
+
+import PropTypes from 'prop-types';
+import Video from 'react-native-video';
+import Orientation from "react-native-orientation";
+
+const screenWidth = Dimensions.get('window').width;
+const defaultVideoHeight = screenWidth * 9/16;
+
+export default class VideoPlayer extends Component {
+
+    static PropTypes = {
+        onChangeOrientation: PropTypes.func,
+        onTapBackButton: PropTypes.func
+    }
+
+    static defaulProps = {
+        videoWidth: screenWidth,    // 默认视频宽度，竖屏下为屏幕宽度
+        videoHeight: defaultVideoHeight, // 默认视频高度，竖屏下为宽度的9/16，使视频保持16：9的宽高比
+        videoURL: '',    // 视频的地址
+        videoCover: '',  // 视频的封面图地址
+        videoTitle: '',  // 视频的标题
+        enableSwitchScreen: true, // 是否允许视频切换大小
+        tag: 0
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            videoWidth: screenWidth,
+            videoHeight: defaultVideoHeight,
+            videoUrl: this.props.videoURL,
+            videoCover: this.props.videoCover,
+            videoTitle: this.props.videoTitle,
+            hasCover: hasCover, // 是否有视频封面
+            isPaused: true,  // 是否暂停，控制视频的播放和暂停
+            duration: 0,     // 视频的时长
+            currentTime: 0,  // 视屏当前播放的时间
+            isFullScreen: false, // 是否全屏
+            isShowControl: false, // 是否显示播放的工具栏
+            isShowVideoCover: hasCover, // 是否显示视频封面
+            playFromBeginning: false, // 视频是否需要从头开始播放
+            isMuted: false,  // 是否静音
+            volume: 1.0,   // 音量大小
+            playRate: 1.0, // 播放速率
+            lastSingleTapTime: 0,   //上次单点击视频区域的时间
+        }
+    }
+    /*播放视频回调*/ 
+
+    _onLoadStart = () =>{
+        console.log('视频开始加载...');
+    }
+
+    _onBuffering = () =>{
+        console.log('视频缓冲中...');
+    }
+
+    _onLoad = (data) =>{
+        console.log('视频加载完成');
+        this.setState({
+            duration: data.duration,
+          });
+    }
+
+    //进度
+    _onProgressChange = (data) =>{
+        if (!this.state.isPaused) {
+            this.setState({
+              currentTime: data.currentTime,
+            })
+        }
+    }
+
+    //视频播放结束触发的方法
+    _onPlayEnd = () => {
+        console.log('播放结束');
+        this.setState({
+        currentTime: 0.0,
+        isPaused: true,
+        pause: true,
+        playFromBeginning: true,
+        isShowVideoCover: this.state.hasCover
+        });
+    };
+
+    _onPlayError = () => {
+        console.log('视频播放失败');
+    };
+
+
+
+    render(){
+        return (
+            <View style={[{width: this.state.videoWidth, height: this.state.videoHeight,backgroundColor:'#000'}, this.props.style]}>
+                 <Video
+                    ref={(ref) => { this.videoRef = ref }}
+                    source={{uri: this.state.videoUrl}}
+                    resizeMode="contain"
+                    rate={1.0}
+                    volume={1.0}
+                    muted={false}
+                    ignoreSilentSwitch={"ignore"}
+                    style={{width: this.state.videoWidth, height: this.state.videoHeight}}
+                    paused={this.state.isPaused}
+                    onLoadStart={this._onLoadStart}
+                    onBuffer={this._onBuffering}
+                    onLoad={this._onLoad}
+                    onProgress={this._onProgressChange}
+                    onEnd={this._onPlayEnd}
+                    onError={this._onPlayError}
+                    playInBackground={false}
+                    playWhenInactive={false}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
--- app/pages/pagesDetail/VideoDetail.js
+++ app/pages/pagesDetail/VideoDetail.js
@@ -10,12 +10,85 @@ import {
    ImageBackground
 } from 'react-native';
 
-export default class NewsDetail extends Component{
+import VideoPlayer from '../../components/VideoPlayer';
+import Orientation from 'react-native-orientation';
+
+const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
+
+export default class VideoDetail extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isFullScreen: false,
+            voteCount: 0,
+
+        }
+    }
+    params = this.props.navigation.state.params.item;
+    isNews = this.params['videoinfo'];
+    voteCount = this.isNews? this.isNews['voteCount']:this.params['votecount'];
+    uri = this.isNews? this.isNews['mp4_url']:this.params['mp4_url'];
+    cover = this.isNews? this.isNews['cover']:this.params['cover'];
+    playCount = this.isNews? this.isNews['playCount']:this.params['playCount'];
+    topicImg = this.isNews? this.isNews['topicImg']:this.params['topicImg'];
+    topicName  = this.isNews? this.isNews['topicName']:this.params['topicName'];
+
+    componentWillMount() {
+        this.setState({ voteCount: this.voteCount});
+    }
+    
+    componentWillUnmount() {
+        Orientation.unlockAllOrientations();
+    }    
+
+    //横竖屏切换
+    _onOrientationChanged = (isFullScreen) => {
+        if (isFullScreen) {
+            Orientation.lockToPortrait();
+        } else {
+            Orientation.lockToLandscape();
+        }
+    };
+
+    _onClickBackButton = () =>{
+        this.props.navigation.goBack();
+    }
+
+    _onChangeLayout =(event) =>{
+        const {x, y, width, height} = event.nativeEvent.layout;
+        let isLandscape = (width > height);
+        if (isLandscape) {
+            this.setState({
+                isFullScreen: true
+            })
+        }else{
+            this.setState({
+                isFullScreen: false
+            })  
+        }
+    }
+
     render() {
+        console.log(this.params)
+
         return (
-            <View>
+            <View style={styles.container} onLayout={this._onChangeLayout}>
+                <VideoPlayer
+                    ref={(ref) => this.videoPlayer = ref}
+                    videoURL={this.uri}
+                    videoTitle={this.params.title}
+                    videoCover={this.cover}
+                    onChangeOrientation={this._onOrientationChanged}
+                    onTapBackButton={this._onClickBackButton}
+                />
                 <Text>视频详情</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+})
